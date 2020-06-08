bbb09dc3ab647a5c4d95ced1c7090b5cfd4b00e5
zhen.an <zhen.an@zilliz.com>
2020-06-08 16:40:07 +0800

    ui show done

--- src/components/DevTool.tsx
+++ src/components/DevTool.tsx
@@ -2,27 +2,23 @@ import React, { useState, useEffect, useRef } from "react";
 import { create } from "../models/sense_editor";
 import { useUIAceKeyboardMode } from "../plugins/use_ui_ace_keyboard_mode";
 import Button from "@material-ui/core/Button";
-import AceEditor from "react-ace";
+import * as CONSTS from "../consts";
 import "./DevTool.scss";
 const inputId = "ConAppInputTextarea";
-const DEFAULT_INPUT_VALUE = `GET _search
-{
-  "query": {
-    "match_all": {}
-  }
-}`;
+const _parseReq = (request: any) => {
+  return JSON.parse(request.split("\n").join(""));
+};
+
 const DevTools = (props: any) => {
   const editorRef = useRef<HTMLDivElement | null>(null);
   const editorInstanceRef: any = useRef(null);
   const [textArea, setTextArea] = useState<HTMLTextAreaElement | null>(null);
   useUIAceKeyboardMode(textArea);
 
-  const getRange = async () => {
+  const getSession = async () => {
     if (editorInstanceRef.current) {
       const editor = editorInstanceRef.current;
-      editor.getRequestRange().then((res: any) => {
-        console.info("xxx", res);
-      });
+      console.info(editor.getCoreEditor().editor.getSession());
     }
   };
   const getRequest = async () => {
@@ -30,6 +26,14 @@ const DevTools = (props: any) => {
       const editor = editorInstanceRef.current;
       editor.getRequest().then((res: any) => {
         console.info("yyy", res);
+      });
+    }
+  };
+  const getRequests = async () => {
+    if (editorInstanceRef.current) {
+      const editor = editorInstanceRef.current;
+      editor.getRequestsInRange().then((res: any) => {
+        console.info("zzz", res);
       });
     }
   };
@@ -43,19 +47,23 @@ const DevTools = (props: any) => {
     if (textareaElement) {
       textareaElement.setAttribute("id", inputId);
     }
-    editor.update(DEFAULT_INPUT_VALUE);
-
+    // set init value for test
+    editor.update(CONSTS.DEFAULT_INPUT_VALUE);
+    editor.getCoreEditor().editor.getSession().setTabSize(11)
     setTextArea(textareaElement);
   }, []);
 
   return (
     <>
       <div id="ConAppEditorActions">
-        <Button variant="contained" onClick={getRange}>
-          Get Range
+        <Button variant="contained" onClick={getSession}>
+          Get Session
         </Button>
         <Button variant="contained" onClick={getRequest}>
           Get Request
+        </Button>
+        <Button variant="contained" onClick={getRequests}>
+          Get Requests
         </Button>
       </div>
       <div ref={editorRef} id="ConAppEditor" />
---
+++ src/consts/index.ts
@@ -0,0 +1,30 @@
+
+export const DEFAULT_INPUT_VALUE = `
+GET _search
+{
+  "query": {
+    "match_all": {}
+  }
+}
+POST _add
+{
+  "a": {
+    "b": "b",
+    "c": "c",
+  }
+}
+POST _add
+{
+  "a": {
+    "e": "b",
+    "f": "c",
+  }
+}
+POST delete
+{
+  "a": {
+    "b": "b",
+    "c": "c",
+  }
+}
+`;
